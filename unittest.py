cc6f399aa8610cc4c0ab3e33fa3024a00768a8ad
JordanQRobinson <38891461+JordanQRobinson@users.noreply.github.com>
2020-07-05 23:42:15 -0500

    Create unittest.py

---
+++ unittest.py
@@ -0,0 +1,15 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+    def test_make_new_user():
+        make_new_user() is None
+    def test_get_user_for_login():
+        get_user_for_login() is None
