bf85b940218f8e7e3dcc8e42c820d110174627c7
Ryan Haney <yoshiquest@gmail.com>
2020-07-14 20:18:59 -0500

    Abstracted the client's main method, so we can prepare for the UI

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 from dsync import connect_to_server, send_message, receive_message, send_message_fn
 import asyncio
 
-async def main():
+async def testfn():
 	testprofiledata = ("someusername", "somename", "somecity", "somestate", "somezip", "someaddr1", "somepassword")
 	connection = await connect_to_server("localhost", 9000, "csprojecttest")
 	await send_message(connection, "Login")
@@ -16,5 +16,8 @@ async def main():
 	await connection[1].wait_closed()
 	print("Done!")
 
+def main():
+	asyncio.run(testfn())
+
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
