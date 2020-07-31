a59a7ddee0ca5f41325c82efda86be94a90a58b3
JordanQRobinson <38891461+JordanQRobinson@users.noreply.github.com>
2020-07-30 23:03:41 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,6 +1,10 @@
 from flask import Flask
 from flask import Flask, flash, redirect, render_template, request, session, abort
 import os
+import asyncio
+from client_calls import *
+
+loop = asyncio.get_event_loop
 
 app = Flask(__name__)
 
@@ -12,3 +16,5 @@ def homepage():
 
 if __name__ == "__main__":
     app.run()
+
+loop.run_until_complete(<client call>)
